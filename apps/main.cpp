e23d522349ab49d3bb459965e724126b7bdc7521
Lee Thompson <leet4th@gmail.com>
2024-08-11 05:03:56 +0000

    add eigen

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,11 +1,23 @@
-
+#include <Eigen/Dense>
 #include <greeter.hpp>
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[]) {
+    // using own lib
     std::string name{"teddy"};
     greeter::Greeter greeter(name);
     std::cout << greeter.greet(greeter::LanguageCode::EN) << std::endl;
+
+    // using eigen
+    Eigen::Matrix2d A;
+    A << 1, 2, 3, 4;
+
+    Eigen::Matrix2d B;
+    B << 5, 6, 7, 8;
+
+    Eigen::Matrix2d C = A + B;
+    std::cout << "Matrix A + B:\n" << C << "\n\n";
+
     return 0;
 }
